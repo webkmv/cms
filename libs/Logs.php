606bc0c5d4290226a8c714136fe50636bfe4f1f8
snakeduse <snakeduse@gmail.com>
2012-04-18 23:39:00 +0400

    реализовал функцию добавления ошибки в лог

---
+++ libs/Logs.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once "lib/config.php";
+require_once "Files.php";
+
+/**
+ * Класс для ведения логов
+ */
+class Logs
+{
+
+  /**
+   * Файл для хранения логов
+   *
+   * @var string
+   */
+  private $fileLogs;
+
+  /**
+   * Инициализируем новый объект логов
+   *
+   * @param string $filLogs файл для хранения логов
+   */
+  public function __construct($fileLogs)
+  {
+    $this->_fileLogs = new Files($fileLogs);
+  }
+  
+  /**
+   * Добавление нового лог сообщения
+   *
+   * @param string $logMessage лог-сообщение
+   * @return void
+   */
+  public function AddLog ($errorMessage)
+  {
+    $dateNow = date("j.m.Y");
+    $timeNow = date("H:i:s");
+    $logMessage = "|".$dateNow." ".$timeNow."| ".$errorMessage;
+    $this->_fileLogs->AppendData($logMessage);
+  }
+}
+?>
