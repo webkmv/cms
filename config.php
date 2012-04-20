c92fb94e6577dbcb41e18d9cf3068c3a7b83ebe1
snakeduse <snakeduse@gmail.com>
2012-04-20 13:06:02 +0400

    добавил путь к моделям админки и представлениям

--- config.php
+++ config.php
@@ -4,5 +4,7 @@ define ('DIR_ROOT', dirname(__FILE__)."/");
 define ('DIR_LIBS', DIR_ROOT."libs/");
 define ('DIR_ADMIN', DIR_ROOT."admin/");
 define ('DIR_ADMIN_CORE', DIR_ADMIN."core/");
+define ('DIR_ADMIN_VIEWS', DIR_ADMIN_CORE."views/");
+define ('DIR_ADMIN_MODELS', DIR_ADMIN_CORE."models/");
 
 ?>
