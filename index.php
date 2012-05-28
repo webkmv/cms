ae5ab86e7f83712e4cfd3ab5762d1f56c50d332c
snakeduse snakeduse <snakeduse@gmail.com>
2012-05-28 14:29:50 +0400

    сделал проверку страницы на публикацию

--- index.php
+++ index.php
@@ -10,22 +10,37 @@ try
 	$modelPage = new ModelPages();
 	$view = new ViewIndex();
 	$modelTemplate = new ModelTemplates();
-	
+
 	if (empty($_GET))
 	{
 		$page = $modelPage->GetPage("index");
-		$view->assign("title", $page["title"]);
-		$view->assign("content", $page["text"]);
-		$view->display($modelTemplate->GetTemplateName($page["id_template"]));	
+		if ($page["published"] == 1)
+		{
+			$view->assign("title", $page["title"]);
+			$view->assign("content", $page["text"]);
+			$view->display($modelTemplate->GetTemplateName($page["id_template"]));
+		}
+		else
+		{
+			die ("Страница не опубликована.");
+		}
 	}
-	
+
 	else if (!empty($_GET["page"]))
 	{
+
 		$url = $_GET["page"];
 		$page = $modelPage->GetPage($url);
-		$view->assign("title", $page["title"]);
-		$view->assign("content", $page["text"]);
-		$view->display($modelTemplate->GetTemplateName($page["id_template"]));
+		if ($page["published"] == 1)
+		{
+			$view->assign("title", $page["title"]);
+			$view->assign("content", $page["text"]);
+			$view->display($modelTemplate->GetTemplateName($page["id_template"]));
+		}
+		else
+		{
+			die ("Страница не опубликована.");
+		}
 	}
 }
 catch (Exception $exc)
